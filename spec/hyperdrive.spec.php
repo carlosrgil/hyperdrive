f06397cc45eeee21cdb84fccd52ef2ce33e2ec76
Josh Habdas <jhabdas@gmail.com>
2017-05-21 17:02:15 +0800

    ci(Travis): test build failure

--- spec/hyperdrive.spec.php
+++ spec/hyperdrive.spec.php
@@ -18,7 +18,7 @@ describe('hyperdrive', function () {
 
     it('does not echo unexpected string', function () {
       $closure = function() { enter_hyperspace('Danger, Will Robinson!'); };
-      expect($closure)->not->toEcho('<script>Warning! Warning!</script>');
+      expect($closure)->not->toEcho('<script>Danger, Will Robinson!</script>');
     });
   });
 });
